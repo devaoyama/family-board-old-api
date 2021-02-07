995e53b5201504d4b083a05211b4fd14dd568fdf
devkeita <keita.mywork@gmail.com>
2021-02-07 14:03:20 +0900

    LIFFのトークン認証を実装

--- line/liff.go
+++ line/liff.go
@@ -1,16 +1,45 @@
 package line
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+)
+
 type Liff struct {
-	Uid string
-	Name string
+	Uid     string
+	Name    string
 	Picture string
 }
 
+type liffVerifiedResponse struct {
+	Sub     string `json:"sub"`
+	Name    string `json:"name"`
+	Picture string `json:"picture"`
+}
+
 func VerifiedIdToken(idToken string) *Liff {
-	// todo:idTokenの有効性を確認する処理を実装する
+	client := &http.Client{
+		Timeout: time.Second * 15,
+	}
+	values := url.Values{}
+	values.Add("id_token", idToken)
+	values.Add("client_id", os.Getenv("LIFF_CLIENT_ID"))
+	req, _ := http.NewRequest("POST", "https://api.line.me/oauth2/v2.1/verify", strings.NewReader(values.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	res, _ := client.Do(req)
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	var response liffVerifiedResponse
+	_ = json.Unmarshal(body, &response)
+
 	return &Liff{
-		Uid: "123456789",
-		Name: "タナカ",
-		Picture: "123456789",
+		Uid:     response.Sub,
+		Name:    response.Name,
+		Picture: response.Picture,
 	}
 }
